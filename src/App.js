9add29d76344f0faa2395c6b9deb77eb3182ffe9
peinutz <pedromarado@gmail.com>
2019-07-09 18:01:56 +0100

    changing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>RWARW</p>
+        <p>Changed!</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
